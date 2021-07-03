838db006dd5655210e390adb1a1356c295cf730b
Luca Bertani <luca.bertani@hotmail.it>
2021-07-03 21:02:55 +0200

    encrypt e decrypt ok! da finire decrypt alternativo

--- 05 AES/main.py
+++ 05 AES/main.py
@@ -1,5 +1,4 @@
-from AESAlgorithm import AESAlgorithm
-from AESAlphabetic import AESAlphabetic
+from AESAlgorithm import process_key, encrypt, decrypt, decrypt_eq
 
 if __name__ == '__main__':
     # aes_alphabetic = AESAlphabetic("resources/cipher.txt")
@@ -9,9 +8,21 @@ if __name__ == '__main__':
     # print(r, r, bin(r))
 
     plain_text = "00112233445566778899aabbccddeeff"
-    key = "000102030405060708090a0b0c0d0e0f"
-    aes = AESAlgorithm(key, plain_text)
-    aes.encrypt()
+    plain_key = "000102030405060708090a0b0c0d0e0f"
+
+    block = process_key(plain_text)
+    key = process_key(plain_key)
+    # encrypt(block, key, 4, 4, 10)
+
+    plain_cipher = "69c4e0d86a7b0430d8cdb78070b4c55a"
+    cipher = process_key(plain_cipher)
+    key = process_key(plain_key)
+
+    # decrypt(cipher, key, 4, 4, 10)
+    decrypt_eq(cipher, key, 4, 4, 10)
+
+    # aes = AESAlgorithm(key, plain_text)
+    # aes.encrypt()
 
 
 
